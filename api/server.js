b502dced4c6b2a6d372e3652bad579a2c65c2824
Jeremiah Thomas <jeremiah.b.thomas12@gmail.com>
2022-08-11 19:37:09 -0400

    endpoints setup

--- api/server.js
+++ api/server.js
@@ -1,7 +1,36 @@
+const users = [
+  { username: "John", password: "1234" },
+  { username: "Jane", password: "5678" },
+];
+
 const express = require("express");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+server.post("/api/users", (req, res) => {
+  users.push(req.body);
+  res.json(req.body);
+});
+
+server.post("/api/login", (req, res) => {
+  users.forEach((user) => {
+    if (
+      user.username === req.body.username &&
+      user.password === req.body.password
+    ) {
+      res.json({ message: "Welcome" });
+    } else {
+      res.json({
+        message: "User not found. PLease check info or create a user.",
+      });
+    }
+  });
+});
 
 module.exports = server;
